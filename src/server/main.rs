08da299f59a9ea8fc633105bfe3547c01c4c3500
Samuel DA MOTA <da.mota.sam@gmail.com>
2024-12-19 14:58:48 +0100

    added message showing on which port the server listens

--- src/server/main.rs
+++ src/server/main.rs
@@ -73,5 +73,6 @@ async fn main() {
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on port 3000");
     axum::serve(listener, app).await.unwrap();
 }
